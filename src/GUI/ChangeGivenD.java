d55125a1b617c01be1432ae4bbcc86032a0532e4
alexlimon <alex.limon@mavs.uta.edu>
2015-11-29 12:25:47 -0600

    fixed change to work in multiple orders

--- src/GUI/ChangeGivenD.java
+++ src/GUI/ChangeGivenD.java
@@ -254,7 +254,9 @@ public class ChangeGivenD extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-dispose();        // TODO add your handling code here:
+          getContentPane().removeAll();
+        dispose(); 
+        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
